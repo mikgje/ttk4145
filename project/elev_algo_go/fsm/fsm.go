49f4517a47c383df84624b0416f5cf20c7c08989
Arnstein Molvig <arnstein.molvig@outlook.com>
2025-01-29 10:16:19 +0100

    Capitalize function names in fsm.go to allow external access

--- project/elev_algo_go/fsm/fsm.go
+++ project/elev_algo_go/fsm/fsm.go
@@ -1,36 +1,36 @@
 package fsm
 
 import (
-	"fmt"
 	"elevator"
+	"elevio"
+	"fmt"
 	"requests"
 	"timer"
-	"elevio"
 )
 
 var elevator_cab elevator.Elevator
 
-func fsm_init() {
-	elevator_cab = elevator.Elevator_uninitialised();
+func Fsm_init() {
+	elevator_cab = elevator.Elevator_uninitialised()
 }
 
-func fsm_set_all_lights(es elevator.Elevator) {
-	for floor:=0; floor < elevator.N_FLOORS; floor++ {
-		for btn:=0; btn < elevator.N_BUTTONS; btn++ {
+func Fsm_set_all_lights(es elevator.Elevator) {
+	for floor := 0; floor < elevator.N_FLOORS; floor++ {
+		for btn := 0; btn < elevator.N_BUTTONS; btn++ {
 			// outputDevice.requestButtonLight(floor, btn, es.Requests[floor][btn])
 			elevio.SetButtonLamp(elevio.ButtonType(btn), floor, bool(es.Requests[floor][btn]))
 		}
 	}
 }
 
-func  fsm_on_init_between_floors() {
+func Fsm_on_init_between_floors() {
 	// outputDevice.motorDirection(elevio.MD_Down)
 	elevio.SetMotorDirection(elevio.MD_Down)
 	elevator_cab.Dirn = elevio.MD_Down
 	elevator_cab.Behaviour = elevator.EB_Moving
 }
 
-func fsm_on_request_button_press(btn_floor int, btn_type elevio.ButtonType) {
+func Fsm_on_request_button_press(btn_floor int, btn_type elevio.ButtonType) {
 	elevator.Elevator_print(elevator_cab)
 
 	switch elevator_cab.Behaviour {
@@ -41,11 +41,11 @@ func fsm_on_request_button_press(btn_floor int, btn_type elevio.ButtonType) {
 			elevator_cab = requests.Requests_clear_at_current_floor(elevator_cab)
 		}
 		break
-	
+
 	case elevator.EB_Moving:
 		elevator_cab.Requests[btn_floor][btn_type] = true
 		break
-	
+
 	case elevator.EB_Idle:
 		elevator_cab.Requests[btn_floor][btn_type] = true
 		pair := requests.Requests_choose_direction(elevator_cab)
@@ -69,7 +69,7 @@ func fsm_on_request_button_press(btn_floor int, btn_type elevio.ButtonType) {
 		}
 	}
 
-	fsm_set_all_lights(elevator_cab)
+	Fsm_set_all_lights(elevator_cab)
 
 	fmt.Println("\nNew state:")
 	elevator.Elevator_print(elevator_cab)
@@ -80,7 +80,7 @@ func fsm_on_floor_arrival(new_floor int) {
 	elevator_cab.Floor = new_floor
 	// outputDevice.floorIndicator(elevator_cab.Floor)
 	elevio.SetFloorIndicator(elevator_cab.Floor)
-	
+
 	switch elevator_cab.Behaviour {
 	case elevator.EB_Moving:
 		if requests.Requests_should_stop(elevator_cab) {
@@ -90,19 +90,19 @@ func fsm_on_floor_arrival(new_floor int) {
 			elevio.SetDoorOpenLamp(true)
 			elevator_cab = requests.Requests_clear_at_current_floor(elevator_cab)
 			timer.Timer_start(elevator_cab.Config.DoorOpenDuration_s)
-			fsm_set_all_lights(elevator_cab)
+			Fsm_set_all_lights(elevator_cab)
 			elevator_cab.Behaviour = elevator.EB_DoorOpen
 		}
 		break
 	default:
-		break;
+		break
 	}
 
 	fmt.Println("\nNew state:")
 	elevator.Elevator_print(elevator_cab)
 }
 
-func fsm_on_door_timeout() {
+func Fsm_on_door_timeout() {
 	elevator.Elevator_print(elevator_cab)
 
 	switch elevator_cab.Behaviour {
@@ -115,7 +115,7 @@ func fsm_on_door_timeout() {
 		case elevator.EB_DoorOpen:
 			timer.Timer_start(elevator_cab.Config.DoorOpenDuration_s)
 			elevator_cab = requests.Requests_clear_at_current_floor(elevator_cab)
-			fsm_set_all_lights(elevator_cab)
+			Fsm_set_all_lights(elevator_cab)
 			break
 		case elevator.EB_Moving:
 			break
@@ -135,4 +135,3 @@ func fsm_on_door_timeout() {
 	fmt.Print("\nNew state:")
 	elevator.Elevator_print(elevator_cab)
 }
-
